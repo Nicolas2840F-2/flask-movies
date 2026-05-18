9888ca40f91b81dac711aea07b73c2500a06d126
Nicolas2840F-2 <ncastanedapardo@gmail.com>
2026-05-17 20:28:41 -0500

    Cargue de información adicional

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 # config.py
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+basedir = Path(__file__).resolve().parent
+load_dotenv(dotenv_path=basedir / '.env')
 
 class Config:
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@viaduct.proxy.rlwy.net:32465/ProyectoPY'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/ProyectoPY'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/ProyectoPY'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:1234@localhost:3306/ProyectoPY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
